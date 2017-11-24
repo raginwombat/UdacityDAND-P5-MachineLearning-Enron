379eb8f700178b5c6c886a376ba59122a7565992
Ryan N <rnels61@wgu.edu>
2017-11-24 15:30:27 -0500

    Classifiers Stubbed out

--- poi_id.py
+++ poi_id.py
@@ -5,25 +5,28 @@ import pickle
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data
+from tester import dump_classifier_and_data, test_classifier
 
 #my adds:
 import matplotlib.pyplot
 from sklearn.cross_validation  import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
-from outlier_cleaner import outlierCleaner
 import operator
 from collections import OrderedDict
+from sklearn.ensemble import AdaBoostClassifier
+from time import time
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.grid_search import GridSearchCV
 
-
+from sklearn.svm import SVC
 
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'salary', 'total_payments', 'bonus', 'restricted_stock_deferred',  'from_poi_frac', 'to_poi_frac',
-	'exercised_stock_options', 'long_term_incentive' ] # You will need to use more features
+features_list = ['poi', 'salary', 'total_payments', 'bonus', 'restricted_stock_deferred',  'from_poi_frac', 'to_poi_frac',	'exercised_stock_options', 'long_term_incentive' ] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -39,13 +42,22 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 	for key in  data_dict[data_dict.keys()[0]].keys():
 		my_data[key] = [0 if per[1][key]=='NaN' else per[1][key] for per in data_dict.items()]		
-	
-	
+
+	#identify outliers and remove them by forcing to 0. can't pop them without screwin up the index:
+	for key in my_data.iterkeys():
+		cutoff = int(round(len(my_data[key]) *.05))
+		print  "removing top ", cutoff,"outliers for ", key
+		if key!='poi':
+			for v in  sorted(my_data[key], reverse=True)[:cutoff]:
+				#print my_data[key].index(v)
+				my_data[key][my_data[key].index(v) ] = 0
+		
+		
 ### Task 3: Create new feature(s)
 #new feature will be % of messages to POI and From POI
 #Using list comprehension to avoid having to initailize array  of proper length for new term
-my_data['from_poi_frac'] = [0 if my_data['from_messages'][i] ==0  else (float(my_data['from_poi_to_this_person'][i]) / float(my_data['from_messages'][i])) for i in range(0, len(my_data['to_messages'])-1)]
-my_data['to_poi_frac'] = [0 if my_data['to_messages'][i]==0 else (float(my_data['from_this_person_to_poi'][i])/ float(my_data['to_messages'][i]))  for i in range(0, len(my_data['to_messages'])-1)]
+my_data['from_poi_frac'] = [0 if my_data['from_messages'][i] ==0  else (float(my_data['from_poi_to_this_person'][i]) / float(my_data['from_messages'][i])) for i in range(0, len(my_data['to_messages']))]
+my_data['to_poi_frac'] = [0 if my_data['to_messages'][i]==0 else (float(my_data['from_this_person_to_poi'][i])/ float(my_data['to_messages'][i]))  for i in range(0, len(my_data['to_messages']))]
 
 
 ### Store to my_dataset for easy export below.
@@ -57,21 +69,33 @@ my_dataset = my_data
 '''due to the previous converstion of dict -> array  I can skip feature format func, 
  I still need to  convert my data to numpy arrays and make data[0]=poi for comapitlity later
 reduce data set to only the ones in the freature list and set data[0] = poi for compatiblity'''
-	
-data = [np.reshape( np.array(my_data['poi']).astype(np.int), (len(my_data['poi']), 1))]
+
+data = [ np.array(my_data['poi']) ]
 
 #extract only features in list
 for key in features_list:
 	if key!='poi':
-		data.append( np.reshape( np.array(my_data[key]).astype(np.int), (len(my_data[key]), 1)))
-data = np.array(data)
+		data.append( np.array(my_data[key]).astype(np.int))
+	if key == 'to_poi_frac' or key == 'from_poi_frac':
+		data.append( np.array(my_data[key]).astype(np.float))
 
 
 
 labels, features = targetFeatureSplit(data)
-#print labels
-#print my_data['poi']
-#print features
+
+
+labels = np.array(my_data['poi']) 
+features = []
+for key in features_list:
+	if key!='poi':
+		features.append( np.array(my_data[key]).astype(np.int))
+	if key == 'to_poi_frac' or key == 'from_poi_frac':
+		features.append( np.array(my_data[key]).astype(np.float))
+
+
+#Currentl shape of features is teatures == rows and people == columns need to transpose for compatiblity
+features = np.transpose(features)
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -79,14 +103,43 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+features_train, features_test, labels_train, labels_test = \
+	train_test_split(features, labels, test_size=0.3, random_state=42)
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-clf.fit(features, labels)
+#create dict for diff classifiers
+clfs = {'classifier':{}, 'result':{}, 'params':{}}
+clfs['classifier']['Niaeve Bayes'] = GaussianNB()
+clfs['classifier']['Ada Boost'] = AdaBoostClassifier(n_estimators =13, learning_rate =1)
+clfs['classifier']['Decision Tree'] = DecisionTreeClassifier(criterion='entropy',min_samples_split=40)
+clfs['classifier']['SVC'] = SVC(C= 10065 , gamma = 'auto', kernel='rbf')
+max_score = 0.0
+'''
 
+clfs['params']['Niaeve Bayes'] = {}
+clfs['params']['Ada Boost'] = 
+clfs['params']['Decision Tree'] = 
+'''
+for clf_key in clfs['classifier'].iterkeys():
+	t0 = time()
+	clfs['classifier'][clf_key].fit(features_train, labels_train)
+	print clf_key
+	print 'Fit took: ', round(time()-t0, 3), 's'
 
+	t0 = time()
+	pred= clfs['classifier'][clf_key].predict(features_test)
+	print 'Prediction took: ', round(time()-t0, 3), 's'
+
+	t0 = time()
+	score = clfs['classifier'][clf_key].score(features_test, labels_test)
+
+	print 'Scoring took: ', round(time()-t0, 3), 's'
+	print 'Accuracy of : ', score
+	clfs['result'][clf_key]=score
+
+print "Max classifier:"
+print max(clfs['result'].iteritems(), key=operator.itemgetter(1))[0]
+
+	#test_classifier(clfs['classifier'][clf_key], dataset, feature_list, folds = 1000)
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
@@ -95,7 +148,7 @@ clf.fit(features, labels)
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
 	train_test_split(features, labels, test_size=0.3, random_state=42)
 
